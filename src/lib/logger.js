69536179b9ae2b47814480c2077f075a908c7ec8
Damien Laureaux <d.laureaux@timoa.com>
2019-08-18 15:56:46 +0200

    Add a logger lib with `correlationId` and datetime that uses Winston

---
+++ src/lib/logger.js
@@ -0,0 +1,28 @@
+const { createLogger, format, transports } = require('winston');
+const uuid = require('uuid');
+
+const env = process.env.NODE_ENV || 'development';
+const correlationId = uuid.v1();
+
+const logger = createLogger({
+  level: env === 'development' ? 'debug' : 'info',
+  format: format.combine(
+    format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss',
+    }),
+    format.json(),
+  ),
+  transports: [
+    new transports.Console({
+      level: 'info',
+      format: format.combine(
+        format.colorize(),
+        format.printf(
+          info => `${correlationId} | ${info.timestamp} | ${info.level}: ${info.message}`,
+        ),
+      ),
+    }),
+  ],
+});
+
+module.exports = logger;
