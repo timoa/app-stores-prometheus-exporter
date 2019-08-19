2aa1b83de7f47f8176bc9e3778f2753efd51e009
Damien Laureaux <d.laureaux@timoa.com>
2019-08-19 10:55:56 +0200

    Fix previous changes on the tests and remove the functional folder

--- test/exporter.spec.js
+++ test/exporter.spec.js
@@ -2,18 +2,26 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const exporter = require('../src/exporter');
 
-const should = chai.should(); // eslint-disable-line
+const { expect } = chai;
 
 // Load the Chai HTTP plugin
 chai.use(chaiHttp);
 
 // Exporter
-describe('Exporter', () => {
+// eslint-disable-next-line func-names
+describe('Exporter', function () {
+  this.timeout(6000);
+
   it('it should returns a valid Prometheus Exporter response', (done) => {
     chai.request(exporter)
       .get('/metrics')
       .end((err, res) => {
-        res.should.have.status(200);
+        expect(res.status).to.have.be.equals(200);
+        expect(res.header['content-type']).to.equal('text/plain; version=0.0.4; charset=utf-8');
+        // expect(res.text).to.be.a('string');
+        expect(res.text).to.match(/(?:process)/);
+        expect(res.text).to.match(/(?:nodejs)/);
+        expect(res.text).to.match(/(?:appstores)/);
         done();
       });
   });
@@ -22,7 +30,9 @@ describe('Exporter', () => {
     chai.request(exporter)
       .get('/')
       .end((err, res) => {
-        res.should.have.status(404);
+        expect(res.status).to.have.be.equals(404);
+        expect(res.header['content-type']).to.equal('text/html');
+        expect(res.text).to.equal('Not Found.');
         done();
       });
   });
@@ -31,7 +41,8 @@ describe('Exporter', () => {
     chai.request(exporter)
       .head('/')
       .end((err, res) => {
-        res.should.have.status(204);
+        expect(res.status).to.have.be.equals(204);
+        expect(res.header['content-type']).to.equal('text/html');
         done();
       });
   });
