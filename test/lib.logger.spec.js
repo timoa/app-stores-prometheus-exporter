acff44bda7e420dedb267f96e785ac1a52b83fdd
Damien Laureaux <d.laureaux@timoa.com>
2019-08-19 19:31:06 +0200

    Small fix on the tests

--- test/lib.logger.spec.js
+++ test/lib.logger.spec.js
@@ -3,36 +3,47 @@ const logger = require('../src/lib/logger');
 
 // Logger
 describe('Logger library', () => {
-  it('expect the "logger.info" to exists and be a function', (done) => {
-    expect(typeof (logger.info)).to.be.equals('function');
-    done();
+  describe('info()', () => {
+    it('expect the "logger.info()" to exists and be a function', (done) => {
+      expect(typeof (logger.info)).to.be.equals('function');
+      done();
+    });
+    it('expect the "logger.info()" function to not throw without an argument', (done) => {
+      expect(logger.info).to.not.throw();
+      done();
+    });
   });
-  it('expect the "logger.info" function to not throw without an argument', (done) => {
-    expect(logger.info).to.not.throw();
-    done();
+
+  describe('warn()', () => {
+    it('expect the "logger.warn()" to exists and be a function', (done) => {
+      expect(typeof (logger.warn)).to.be.equals('function');
+      done();
+    });
+    it('expect the "logger.warn()" function to not throw without an argument', (done) => {
+      expect(logger.warn).to.not.throw();
+      done();
+    });
   });
-  it('expect the "logger.warn" to exists and be a function', (done) => {
-    expect(typeof (logger.warn)).to.be.equals('function');
-    done();
+
+  describe('error()', () => {
+    it('expect the "logger.error()" to exists and be a function', (done) => {
+      expect(typeof (logger.error)).to.be.equals('function');
+      done();
+    });
+    it('expect the "logger.error()" function to not throw without an argument', (done) => {
+      expect(logger.error).to.not.throw();
+      done();
+    });
   });
-  it('expect the "logger.warn" function to not throw without an argument', (done) => {
-    expect(logger.warn).to.not.throw();
-    done();
-  });
-  it('expect the "logger.error" to exists and be a function', (done) => {
-    expect(typeof (logger.error)).to.be.equals('function');
-    done();
-  });
-  it('expect the "logger.error" function to not throw without an argument', (done) => {
-    expect(logger.error).to.not.throw();
-    done();
-  });
-  it('expect the "logger.log" to exists and be a function', (done) => {
-    expect(typeof (logger.log)).to.be.equals('function');
-    done();
-  });
-  it('expect the "logger.log" function to throw without an argument', (done) => {
-    expect(logger.log).to.throw();
-    done();
+
+  describe('log()', () => {
+    it('expect the "logger.log()" to exists and be a function', (done) => {
+      expect(typeof (logger.log)).to.be.equals('function');
+      done();
+    });
+    it('expect the "logger.log()" function to throw without an argument', (done) => {
+      expect(logger.log).to.throw();
+      done();
+    });
   });
 });
