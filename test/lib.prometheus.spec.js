a0d0dd941fb1daf0324eb0df6723b24b9f952d74
Damien Laureaux <d.laureaux@timoa.com>
2022-04-12 15:27:34 +0200

    refactor(tests): refactor unit-tests and add chai-as-promise to add better coverage

--- test/lib.prometheus.spec.js
+++ test/lib.prometheus.spec.js
@@ -1,38 +1,44 @@
-const { expect } = require('chai');
+const chai = require('chai');
+const expect = chai.expect;
+const chaiAsPromised = require('chai-as-promised');
+
 const prometheus = require('../src/lib/prometheus');
+
+// Load the Chai plugins
+chai.use(chaiAsPromised);
 
 // Prometheus
 describe('Prometheus library', () => {
   describe('init()', () => {
-    it('expect the "prometheus.init()" to exists and be a function', (done) => {
-      expect(typeof (prometheus.init)).to.be.equals('function');
-      done();
+    it('expect the "prometheus.init()" to exists and be a function', () => {
+      return expect(typeof (prometheus.init)).to.be.equals('function');
     });
-    it('expect the "prometheus.init()" function to not throw', (done) => {
-      expect(prometheus.init).to.not.throw();
-      done();
+    it('expect the "prometheus.init()" function to not throw', () => {
+      return expect(prometheus.init).to.not.throw();
     });
   });
 
   describe('getMetrics()', () => {
-    it('expect the "prometheus.getMetrics()" to exists and be a function', (done) => {
-      expect(typeof (prometheus.getMetrics)).to.be.equals('function');
-      done();
+    it('expect the "prometheus.getMetrics()" to exists and be a function', () => {
+      return expect(typeof (prometheus.getMetrics)).to.be.equals('function');
     });
-    it('expect the "prometheus.getMetric()" function to not throw', (done) => {
-      expect(prometheus.getMetrics).to.not.throw();
-      done();
+    it('expect the "prometheus.getMetric()" function to not throw', () => {
+      return expect(prometheus.getMetrics).to.not.throw();
+    });
+    it('expect the "prometheus.getMetric()" function to resolves as promised', () => {
+      return expect(Promise.resolve(prometheus.getMetrics)).to.be.fulfilled;
+    });
+    it('expect the "prometheus.getMetric()" function to rejects as promised', () => {
+      return expect(Promise.reject(prometheus.getMetrics)).to.be.rejected;
     });
   });
 
   describe('getContentType()', () => {
-    it('expect the "prometheus.getContentType()" to exists and be a string', (done) => {
-      expect(typeof (prometheus.getContentType)).to.be.equals('string');
-      done();
+    it('expect the "prometheus.getContentType()" to exists and be a string', () => {
+      return expect(typeof (prometheus.getContentType)).to.be.equals('string');
     });
-    it('expect the "prometheus.getContentType()" to return the Prometheus Exporter content-type', (done) => {
-      expect(prometheus.getContentType).to.be.equals('text/plain; version=0.0.4; charset=utf-8');
-      done();
+    it('expect the "prometheus.getContentType()" to return the Prometheus Exporter content-type', () => {
+      return expect(prometheus.getContentType).to.be.equals('text/plain; version=0.0.4; charset=utf-8');
     });
   });
 });
