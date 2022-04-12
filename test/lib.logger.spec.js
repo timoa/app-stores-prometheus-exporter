515a1f52484a164f3b2d3aab6d420d3f542f1ea5
Damien Laureaux <d.laureaux@timoa.com>
2022-04-12 15:30:08 +0200

    chore(lint): fix lint issues

--- test/lib.logger.spec.js
+++ test/lib.logger.spec.js
@@ -4,38 +4,22 @@ const logger = require('../src/lib/logger');
 // Logger
 describe('Logger library', () => {
   describe('info()', () => {
-    it('expect the "logger.info()" to exists and be a function', () => {
-      return expect(typeof (logger.info)).to.be.equals('function');
-    });
-    it('expect the "logger.info()" function to not throw without an argument', () => {
-      return expect(logger.info).to.not.throw();
-    });
+    it('expect the "logger.info()" to exists and be a function', () => expect(typeof (logger.info)).to.be.equals('function'));
+    it('expect the "logger.info()" function to not throw without an argument', () => expect(logger.info).to.not.throw());
   });
 
   describe('warn()', () => {
-    it('expect the "logger.warn()" to exists and be a function', () => {
-      return expect(typeof (logger.warn)).to.be.equals('function');
-    });
-    it('expect the "logger.warn()" function to not throw without an argument', () => {
-      return expect(logger.warn).to.not.throw();
-    });
+    it('expect the "logger.warn()" to exists and be a function', () => expect(typeof (logger.warn)).to.be.equals('function'));
+    it('expect the "logger.warn()" function to not throw without an argument', () => expect(logger.warn).to.not.throw());
   });
 
   describe('error()', () => {
-    it('expect the "logger.error()" to exists and be a function', () => {
-      return expect(typeof (logger.error)).to.be.equals('function');
-    });
-    it('expect the "logger.error()" function to not throw without an argument', () => {
-      return expect(logger.error).to.not.throw();
-    });
+    it('expect the "logger.error()" to exists and be a function', () => expect(typeof (logger.error)).to.be.equals('function'));
+    it('expect the "logger.error()" function to not throw without an argument', () => expect(logger.error).to.not.throw());
   });
 
   describe('log()', () => {
-    it('expect the "logger.log()" to exists and be a function', () => {
-      return expect(typeof (logger.log)).to.be.equals('function');
-    });
-    it('expect the "logger.log()" function to throw without an argument', () => {
-      return expect(logger.log).to.throw();
-    });
+    it('expect the "logger.log()" to exists and be a function', () => expect(typeof (logger.log)).to.be.equals('function'));
+    it('expect the "logger.log()" function to throw without an argument', () => expect(logger.log).to.throw());
   });
 });
--- test/lib.prometheus.spec.js
+++ test/lib.prometheus.spec.js
@@ -1,5 +1,6 @@
 const chai = require('chai');
-const expect = chai.expect;
+
+const { expect } = chai;
 const chaiAsPromised = require('chai-as-promised');
 
 const prometheus = require('../src/lib/prometheus');
@@ -10,35 +11,21 @@ chai.use(chaiAsPromised);
 // Prometheus
 describe('Prometheus library', () => {
   describe('init()', () => {
-    it('expect the "prometheus.init()" to exists and be a function', () => {
-      return expect(typeof (prometheus.init)).to.be.equals('function');
-    });
-    it('expect the "prometheus.init()" function to not throw', () => {
-      return expect(prometheus.init).to.not.throw();
-    });
+    it('expect the "prometheus.init()" to exists and be a function', () => expect(typeof prometheus.init).to.be.equals('function'));
+    it('expect the "prometheus.init()" function to not throw', () => expect(prometheus.init).to.not.throw());
   });
 
   describe('getMetrics()', () => {
-    it('expect the "prometheus.getMetrics()" to exists and be a function', () => {
-      return expect(typeof (prometheus.getMetrics)).to.be.equals('function');
-    });
-    it('expect the "prometheus.getMetric()" function to not throw', () => {
-      return expect(prometheus.getMetrics).to.not.throw();
-    });
-    it('expect the "prometheus.getMetric()" function to resolves as promised', () => {
-      return expect(Promise.resolve(prometheus.getMetrics)).to.be.fulfilled;
-    });
-    it('expect the "prometheus.getMetric()" function to rejects as promised', () => {
-      return expect(Promise.reject(prometheus.getMetrics)).to.be.rejected;
-    });
+    it('expect the "prometheus.getMetrics()" to exists and be a function', () => expect(typeof prometheus.getMetrics).to.be.equals('function'));
+    it('expect the "prometheus.getMetric()" function to not throw', () => expect(prometheus.getMetrics).to.not.throw());
+    it('expect the "prometheus.getMetric()" function to resolves as promised', () => expect(Promise.resolve(prometheus.getMetrics)).to.be.fulfilled);
+    it('expect the "prometheus.getMetric()" function to rejects as promised', () => expect(Promise.reject(prometheus.getMetrics)).to.be.rejected);
   });
 
   describe('getContentType()', () => {
-    it('expect the "prometheus.getContentType()" to exists and be a string', () => {
-      return expect(typeof (prometheus.getContentType)).to.be.equals('string');
-    });
-    it('expect the "prometheus.getContentType()" to return the Prometheus Exporter content-type', () => {
-      return expect(prometheus.getContentType).to.be.equals('text/plain; version=0.0.4; charset=utf-8');
-    });
+    it('expect the "prometheus.getContentType()" to exists and be a string', () => expect(typeof prometheus.getContentType).to.be.equals('string'));
+    it('expect the "prometheus.getContentType()" to return the Prometheus Exporter content-type', () => expect(prometheus.getContentType).to.be.equals(
+      'text/plain; version=0.0.4; charset=utf-8',
+    ));
   });
 });
